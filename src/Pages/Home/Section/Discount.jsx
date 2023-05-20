0f0f7eb75a6484245343421e611e955d1fb3c6fb
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-05-20 19:03:26 +0600

    add discound banner section

--- src/Pages/Home/Section/Discount.jsx
+++ src/Pages/Home/Section/Discount.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
-
+import React from "react";
+import imag1 from "../../../assets/istockphoto-1069933778-170667a.jpg";
+import imag2 from "../../../assets/istockphoto-1359811855-170667a.jpg";
 const Discount = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="flex mx-auto gap-4 pt-10 pb-32">
+      <div className="w-1/2 relative ">
+        <img className="w-full hover:scale-110 rounded transition-all duration-300" src={imag2} alt="" />
+        <div className="absolute top-20 right-8">
+          <p className="text-3xl font-bold"><span className="font-extrabold">Collation</span> 
+            <span className="block mt-3">Flat  30% off</span>
+          </p>
+          <button className="btn btn-sm mt-5 ">shop now</button>
         </div>
-    );
+      </div>
+      <div className="w-1/2 relative ">
+        <img className="w-full hover:scale-110 rounded transition-all duration-300" src={imag1} alt="" />
+        <div className="absolute top-20 right-8">
+          <p className="text-3xl font-bold"><span className="font-extrabold">Collation</span> 
+            <span className="block mt-3">Flat  30% off</span>
+          </p>
+          <button className="btn btn-sm mt-5 ">shop now</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Discount;
